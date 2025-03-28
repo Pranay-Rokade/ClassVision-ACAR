a2d54863e8373f6e01adca3ee5b59261a5ee5b75
Pranay-Rokade <pranayrokade86@gmail.com>
2025-03-28 18:46:00 +0530

    Fixed Errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,7 @@ import Hero from './components/Hero'
 import Login from './components/Login'
 import Signup from './components/Signup' 
 import Live from './components/Live'
-<<<<<<< HEAD
 import Upload from './components/Upload';
-=======
-import VideoStream from './components/Try';
->>>>>>> d2788a034112bb3a8f2abca0cc1b95cc53ccf3ed
 const App = () => {
 
   return (
@@ -23,11 +19,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/live" element={<Live />} />
-<<<<<<< HEAD
           <Route path="/upload" element={<Upload/>} />
-=======
-          <Route path="/stream" element={<VideoStream />} />
->>>>>>> d2788a034112bb3a8f2abca0cc1b95cc53ccf3ed
         </Routes>
         <Footer />
       </div>
