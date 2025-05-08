43e8a737e88961302f5e2f6cb5cbc7376f594aab
yashtapre77 <yashtapre77@gmail.com>
2025-05-08 12:11:47 +0530

    storing encodings in pickle file

--- ml/students_action_logging_2.py
+++ ml/students_action_logging_2.py
@@ -9,7 +9,8 @@ import pickle
 def face_encodings_for_dataset(image_data_path = "images"):
     encodings = {}
     for filename in os.listdir(image_data_path):
-        name = os.path.splitext(filename,'.')[0]
+        name = os.path.splitext(filename)[0]
+        print(f"Processing {filename}, {name}")
         img = face_recognition.load_image_file(os.path.join(image_data_path, filename))
         encodings[name] = face_recognition.face_encodings(img)[0]
     
@@ -18,3 +19,5 @@ def face_encodings_for_dataset(image_data_path = "images"):
 
     return encodings
 
+encodings = face_encodings_for_dataset()
+print(encodings)
