6974ae0416f8dc392e16a39dd305427b442d9171
yashtapre77 <yashtapre77@gmail.com>
2025-05-08 10:14:29 +0530

    initiated face recognition

---
+++ ml/students_action_logging_2.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+from datetime import datetime 
+import csv
+import os
+import face_recognition
+import pickle
+
+def face_encodings_for_dataset(image_data_path = "images"):
+    encodings = {}
+    for filename in os.listdir(image_data_path):
+        name = os.path.splitext(filename,'.')[0]
+        img = face_recognition.load_image_file(os.path.join(image_data_path, filename))
+        encodings[name] = face_recognition.face_encodings(img)[0]
+    
+    with open('encodings.pickle', 'wb') as f:
+        pickle.dump(encodings, f)
+
+    return encodings
+
