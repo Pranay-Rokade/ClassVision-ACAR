27e07735dea248a07e6ad3d52cfc3c891164fd7f
yashtapre77 <yashtapre77@gmail.com>
2025-05-08 17:25:44 +0530

    model for storing sessions

--- backend/analysis/admin.py
+++ backend/analysis/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from analysis.models import ClassData
 
 # Register your models here.
+admin.site.register(ClassData)
--- backend/analysis/models.py
+++ backend/analysis/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class ClassData(models.Model):
+    MODES = [('teacher', 'Teacher'), ('online', 'Online'), ('student', 'Student'), ('cheating', 'Cheating'), ('hybrid', 'Hybrid')]
+    class_name = models.CharField(max_length=100)
+    csv_file = models.FileField(upload_to='csv_uploads/')
+    description = models.TextField(blank=True, null=True)
+    class_time = models.DateTimeField(auto_now_add=True)
+    mode = models.CharField(max_length=50, choices=MODES, default='student')
+
+    def __str__(self):
+        return f"{self.class_name} - {self.csv_file.name}"
